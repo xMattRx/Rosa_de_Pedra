f837c529cca803ffae5dc9faed692db0ca55669e
Matthew Rachid Romie Nascimento <matthewromie@hotmail.com>
2022-03-08 23:40:14 -0300

    topic javascript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -8,9 +8,7 @@ function handle(number){
       if(rotate === ""){
         img.style.transform = "rotate(180deg)"
         element.children[1].style.display = "none"
-        element.style.height = "93px"
-        
-          
+        element.style.height = "max-content"
       }else{
         img.style.transform = ""
         element.children[1].style.display = "block"
@@ -19,3 +17,27 @@ function handle(number){
     }
   });
 };
+
+function initial(number){
+  document.querySelectorAll("img").forEach(function(img) {
+    const id = img.id;
+    const rotate = img.style.transform
+    if(id == number){
+      const topic = "topic" + number
+      const element = document.getElementById(topic);
+      if(number === 2 || number === 3 || number === 4){
+        img.style.transform = "rotate(180deg)"
+        element.children[1].style.display = "none"
+        element.style.height = "max-content"
+      }else{
+        img.style.transform = ""
+        element.children[1].style.display = "block"
+        element.style.height = "max-content"
+      }
+    }
+  });
+}
+initial(1)
+initial(2)
+initial(3)
+initial(4)
